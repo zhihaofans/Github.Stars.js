7e58674eee0d0d9b3af6f96fdda50e32ed5005c4
zhihaofans <a@zhihaofans.com>
2016-11-30 18:36:52 +0800

    Update Github.desktop.stars.js

--- Github.desktop.stars.js
+++ Github.desktop.stars.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Github.desktop.stars.js
 // @namespace    http://zhihaofans.com
-// @version      0.0.1
+// @version      0.0.2
 // @description  在Github首页显示stars列表
 // @author       zhihaofans
 // @match        https://github.com/
